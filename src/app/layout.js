6e4926efac45d81387408699a66a39fd9709f05f
JoakoMI <joaquinmarchione@gmail.com>
2024-05-12 00:31:13 -0300

    Add: NextUI library components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
