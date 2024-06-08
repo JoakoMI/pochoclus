d227d24384e24b91b8f96a700eef2e0111895037
capricho 123 <diddicorreo@gmail.com>
2024-06-07 23:19:11 -0300

    Movie data, routes Added

--- pochoclus-api/data/movie.js
+++ pochoclus-api/data/movie.js
@@ -1,17 +1,45 @@
-import getConnection from './connection.js';
-const DATABASE = 'Pochoclus';
-const MOVIES = 'Movies';
+import { ObjectId } from "mongodb";
+import getConnection from "./connection.js";
+const DATABASE = "Pochoclus";
+const MOVIES = "Movies";
 
 async function getAllMovies(pageSize, page) {
-	const connectiondb = await getConnection();
-	const movies = await connectiondb
-		.db(DATABASE)
-		.collection(MOVIES)
-		.find({})
-		.limit(pageSize)
-		.skip(pageSize * page)
-		.toArray();
-	return movies;
+  const connectiondb = await getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({})
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return movies;
 }
 
-export { getAllMovies };
+async function getMovieById(movieId) {
+  try {
+    const connectiondb = await getConnection();
+    const movie = await connectiondb
+      .db(DATABASE)
+      .collection(MOVIES)
+      .findOne({ _id: new ObjectId(movieId) });
+
+    return movie;
+  } catch (error) {
+    return "No movie found with that ID";
+  }
+}
+
+async function getMoviesByName(pageSize, page) {
+  const connectiondb = await getConnection();
+  const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({})
+    .limit(pageSize)
+    .skip(pageSize * page)
+	.project({ _id: 1, name: 1, tmdbId: 1 })
+	
+    .toArray();
+  return movies;
+}
+export { getAllMovies, getMovieById, getMoviesByName };
