e2587034d9491c3a44f5ae00fc8485d0dda3a934
ginofrigoni <ginofrigoni@gmail.com>
2024-06-20 17:55:33 -0300

    Responsive

--- pochoclus-ui/src/app/movie/[id]/page.js
+++ pochoclus-ui/src/app/movie/[id]/page.js
@@ -25,63 +25,59 @@ export default function MovieDetail({ params }) {
   if (!movie) return <p className="text-white">Movie not found</p>;
 
   return (
-    <div className="flex flex-col lg:flex-row justify-center items-center">
-      <div className="flex flex-col lg:w-1/2 m-4">
-        <div className="flex flex-col lg:flex-row items-center lg:justify-between">
-          <div className="flex flex-col">
-            <h1 className="text-white font-bold text-4xl mb-2 lg:mb-0">
-              {movie.name}
-            </h1>
-            <h1 className="text-gray-200 font-light text-4xl">
-              ({movie.year})
-            </h1>
-          </div>
+    <div className="flex flex-col items-center bg-dark p-4">
+      <div className="w-full lg:w-1/2 m-4">
+        <div className="flex flex-col items-center">
+          <h1 className="text-white font-bold text-4xl mb-2">
+            {movie.name}
+          </h1>
+          <h2 className="text-gray-300 text-xl"><span className="font-bold text-xl">Año:</span> {movie.year}</h2>
+        </div>
+        <div className="flex flex-col lg:flex-row items-center mt-4">
           <img
             src={movie.poster}
             className="w-full lg:w-64"
             alt={`${movie.name} poster`}
           />
-        </div>
-        <p className="text-white font-light mb-4">{movie.plot}</p>
-        <div className="flex flex-wrap mb-4">
-          <div className="flex flex-col me-8 text-white">
-            <span className="font-bold">Dirige:</span>
-            {movie.directors.map((d, index) => (
-              <a
-                key={d.name}
-                href={`/person/${encodeURIComponent(d.name)}`}
-                className="text-blue-400 hover:underline transition duration-300 ease-in-out mt-1"
-              >
-                {d.name}
-              </a>
-            ))}
+          <div className="text-white mt-4 lg:mt-0 lg:ml-4">
+            <div className="mb-4">
+              <span className="font-bold text-xl">Géneros:</span>
+              <div className="flex flex-wrap">
+                {movie.genres.map((g, index) => (
+                  <span className="font-light ml-2 text-lg" key={g}>
+                    {g}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <p className="font-light text-lg">
+              <span className="font-bold">Sinopsis:</span> {movie.plot}
+            </p>
+            <div className="mt-4">
+              <span className="font-bold text-xl">Actores:</span>
+              <div className="flex flex-wrap">
+                {movie.cast.map((c, index) => (
+                  <a
+                    key={c.name}
+                    href={`/person/${encodeURIComponent(c.name)}`}
+                    className="text-blue-400 hover:underline transition duration-300 ease-in-out ml-2 text-lg"
+                  >
+                    {c.name}
+                    {index < movie.cast.length - 1 && ", "}
+                  </a>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col text-white mt-2 lg:mt-0">
-            <span className="font-bold">Géneros:</span>
-            {movie.genres.map((g, index) => (
-              <span className="font-light ml-2" key={g}>
-                {g}
-              </span>
-            ))}
-          </div>
         </div>
-        <section className="text-white">
-          <span className="font-bold">Actúan:</span>
-          {movie.cast.map((c, index) => (
-            <a
-              key={c.name}
-              href={`/person/${encodeURIComponent(c.name)}`}
-              className="text-blue-400 hover:underline transition duration-300 ease-in-out ml-2"
-            >
-              {c.name}
-              {index < movie.cast.length - 1 && ", "}
-            </a>
-          ))}
-        </section>
       </div>
-      <div className="w-full">
-        <VideoEmbeed videoUrl={movie.link} />
+      <div className="w-full mt-4">
+
+        <div className="flex justify-center">
+          <VideoEmbeed videoUrl={movie.link} />
+        </div>
       </div>
     </div>
   );
 }
+
