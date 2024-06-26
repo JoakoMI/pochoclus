e0b48f45172fcee71ef14c04caebf12a4aaa81b8
JoakoMI <joaquinmarchione@gmail.com>
2024-06-26 17:51:23 -0300

    Add: Login / Signup redirecition when logged

--- pochoclus-ui/src/app/login/page.js
+++ pochoclus-ui/src/app/login/page.js
@@ -14,6 +14,72 @@ export default function Login() {
   });
 
   useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      router.push("/");
+    } else {
+      return (
+        <div className="m-16">
+          <form className="max-w-sm mx-auto m-8 " onSubmit={handleSubmit}>
+            <div class="mb-5">
+              <label
+                htmlFor="email"
+                class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+              >
+                Tu email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="petru@pochoclus.com"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div class="mb-5">
+              <label
+                htmlFor="password"
+                class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+              >
+                Tu contraseña
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Ingresar
+              </button>
+              <h1 className="text-red-600 text-center m-4">{msg}</h1>
+
+              <div class="ms-2 m-8 text-sm text-center font-medium text-gray-200 dark:text-gray-300">
+                Si todavia no tenes cuenta podes{" "}
+                <Link
+                  href="/signup"
+                  class="text-blue-500 hover:underline dark:text-blue-500"
+                >
+                  registrarte acá
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      );
+    }
+  }, []);
+
+  useEffect(() => {
     if (error) {
       setMsg("Credenciales no válidas");
     }
@@ -57,64 +123,4 @@ export default function Login() {
       setError(true);
     }
   };
-
-  return (
-    <div className="m-16">
-      <form className="max-w-sm mx-auto m-8 " onSubmit={handleSubmit}>
-        <div class="mb-5">
-          <label
-            htmlFor="email"
-            class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-          >
-            Tu email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="petru@pochoclus.com"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div class="mb-5">
-          <label
-            htmlFor="password"
-            class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-          >
-            Tu contraseña
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-            onChange={handleChange}
-          />
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Ingresar
-          </button>
-          <h1 className="text-red-600 text-center m-4">{msg}</h1>
-
-          <div class="ms-2 m-8 text-sm text-center font-medium text-gray-200 dark:text-gray-300">
-            Si todavia no tenes cuenta podes{" "}
-            <Link
-              href="/signup"
-              class="text-blue-500 hover:underline dark:text-blue-500"
-            >
-              registrarte acá
-            </Link>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
 }
--- pochoclus-ui/src/app/signup/page.js
+++ pochoclus-ui/src/app/signup/page.js
@@ -1,9 +1,11 @@
 "use client";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
+  const router = useRouter();
   const [errorValue, setErrorValue] = useState(false);
-  const [msg, setMsg] = useState("  Hola ");
+  const [msg, setMsg] = useState("   ");
 
   const [formData, setFormData] = useState({
     email: "",
@@ -13,8 +15,101 @@ export default function Signup() {
   });
 
   useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      router.push("/");
+    } else {
+      return (
+        <form className="max-w-sm mx-auto m-16" onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+            >
+              Tu email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="petru@pochoclus.com"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+            >
+              Nombre de usuario
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Kingbara"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+            >
+              Tu contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="contraseña"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="repeatPassword"
+              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+            >
+              Repetí tu contraseña
+            </label>
+            <input
+              type="password"
+              id="repeatPassword"
+              name="repeatPassword"
+              placeholder="contraseña"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              value={formData.repeatPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="flex items-start mb-5"></div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Registrarme
+          </button>
+          <h1 className="text-red-600 text-center m-4">{msg}</h1>
+        </form>
+      );
+    }
+  });
+
+  useEffect(() => {
     if (errorValue) {
-      setMsg("Hay un error en el formulario. Revise los campos y vuelva a intentar.");
+      setMsg(
+        "Hay un error en el formulario. Revise los campos y vuelva a intentar."
+      );
     }
   }, [errorValue]);
 
@@ -51,77 +146,4 @@ export default function Signup() {
       setErrorValue(true);
     }
   };
-
-  return (
-    <form className="max-w-sm mx-auto m-16" onSubmit={handleSubmit}>
-      <div className="mb-5">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-200 dark:text-white">
-          Tu email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="petru@pochoclus.com"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="mb-5">
-        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-200 dark:text-white">
-          Nombre de usuario
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Kingbara"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="mb-5">
-        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-200 dark:text-white">
-          Tu contraseña
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="contraseña"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="mb-5">
-        <label htmlFor="repeatPassword" className="block mb-2 text-sm font-medium text-gray-200 dark:text-white">
-          Repetí tu contraseña
-        </label>
-        <input
-          type="password"
-          id="repeatPassword"
-          name="repeatPassword"
-          placeholder="contraseña"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          value={formData.repeatPassword}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <h1 className="text-red-600 text-center m-4">{msg}</h1>
-      <div className="flex items-start mb-5"></div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Registrarme
-      </button>
-    </form>
-  );
 }
