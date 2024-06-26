c0da45154985e8e5be4222fbd9dad63df3c2b879
JoakoMI <joaquinmarchione@gmail.com>
2024-06-26 16:22:54 -0300

    Fix: Signup form ui

--- pochoclus-ui/src/app/signup/page.js
+++ pochoclus-ui/src/app/signup/page.js
@@ -114,19 +114,7 @@ export default function Signup() {
           required
         />
       </div>
-      <div className="flex items-start mb-5">
-        <div className="flex items-center h-5">
-          <input
-            id="terms"
-            name="terms"
-            type="checkbox"
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-            checked={formData.terms}
-            onChange={handleChange}
-            required
-          />
-        </div>
-      </div>
+      <div className="flex items-start mb-5"></div>
       <button
         type="submit"
         className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
