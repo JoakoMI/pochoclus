004592dddcd24e6b5c3a42729f992266b6ddf25c
nicobarak <baraknicolas@gmail.com>
2024-06-26 20:07:42 -0300

    Added login/signup is a conditional render, and there is now a "Mi cuenta" button with the funcionality of the previous version

--- pochoclus-ui/src/app/components/NavBar.jsx
+++ pochoclus-ui/src/app/components/NavBar.jsx
@@ -1,25 +1,20 @@
 "use client";
-import React from "react";
-import { useRouter } from "next/navigation.js";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Input,
-} from "@nextui-org/react";
-import { AcmeLogo } from "./AcmeLogo.jsx";
-import { SearchIcon } from "./SearchIcon.jsx";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Menu } from "@headlessui/react";
+import { Navbar, NavbarContent, NavbarItem, Link, Input } from "@nextui-org/react";
 
-export default function  App() {
+export default function App() {
   const router = useRouter();
   const [session, setSession] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const token = localStorage.getItem("authToken");
-    setSession(token);
+    if (token) {
+      setSession(token);
+    }
+    setIsLoading(false);
   }, []);
 
   const handleLogout = () => {
@@ -30,59 +25,85 @@ export default function  App() {
 
   return (
     <Navbar isBordered>
-      <NavbarContent justify="center">
-        <NavbarBrand className="mr-4">
-          <AcmeLogo />
-          <p className="hidden sm:block font-bold text-inherit">Pochoclus</p>
-        </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-3">
-          <NavbarItem>
-            <Link color="foreground" href="/">
-              Home
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="/milista">
-              Mi lista
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="/explorar">
-              Explorar
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="/colecciones">
-              Colecciones
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
+      <NavbarContent justify="start">
+        <p className="hidden sm:block font-bold text-inherit">Pochoclus</p>
       </NavbarContent>
 
-      <NavbarContent as="div" className="items-center" justify="end">
-        <Input
-          classNames={{
-            base: "max-w-full sm:max-w-[10rem] h-10",
-            mainWrapper: "h-full",
-            input: "text-small",
-            inputWrapper:
-              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-          }}
-          placeholder="Buscar..."
-          size="sm"
-          startContent={<SearchIcon size={18} />}
-          type="search"
-        />
+      <NavbarContent justify="start">
+        <NavbarItem>
+          <Link color="foreground" href="/">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/explorar">
+            Explorar
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Input
+            classNames={{
+              base: "max-w-full sm:max-w-[10rem] h-10",
+              mainWrapper: "h-full",
+              input: "text-small",
+              inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            }}
+            placeholder="Buscar..."
+            size="sm"
+            type="search"
+          />
+        </NavbarItem>
+      </NavbarContent>
 
-        {session == null ? (
-          <Link href="/login" placement="bottom-end">
-            Ingresar
-          </Link>
-        ) : (
-          <Link onClick={handleLogout} href="/" placement="bottom-end">
-            Salir
-          </Link>
-        )}
+      <NavbarContent justify="end">
+        <Menu as="div" className="relative">
+          <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+            Mi cuenta
+          </Menu.Button>
+          <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1">
+              {!isLoading && (
+                <>
+                  {session ? (
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button className={`${active ? "bg-gray-900 text-white" : "text-gray-900"} group flex rounded-md items-center w-full px-2 py-2 text-sm`} onClick={handleLogout}>
+                          Salir
+                        </button>
+                      )}
+                    </Menu.Item>
+                  ) : (
+                    <>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link href="/login" className={`${active ? "bg-gray-900 text-white" : "text-gray-900"} group flex rounded-md items-center w-full px-2 py-2 text-sm`}>
+                            Ingresar
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link href="/signup" className={`${active ? "bg-gray-900 text-white" : "text-gray-900"} group flex rounded-md items-center w-full px-2 py-2 text-sm`}>
+                            Registrarse
+                          </Link>
+                        )}
+                      </Menu.Item>
+                    </>
+                  )}
+                  {session && (
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link href="/milista" className={`${active ? "bg-gray-900 text-white" : "text-gray-900"} group flex rounded-md items-center w-full px-2 py-2 text-sm`}>
+                          Mi lista
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  )}
+                </>
+              )}
+            </div>
+          </Menu.Items>
+        </Menu>
       </NavbarContent>
     </Navbar>
   );
--- pochoclus-ui/src/app/signup/page.js
+++ pochoclus-ui/src/app/signup/page.js
@@ -6,6 +6,7 @@ export default function Signup() {
   const router = useRouter();
   const [errorValue, setErrorValue] = useState(false);
   const [msg, setMsg] = useState("   ");
+  const [isLoading, setIsLoading] = useState(true); 
 
   const [formData, setFormData] = useState({
     email: "",
@@ -15,93 +16,17 @@ export default function Signup() {
   });
 
   useEffect(() => {
-    if (localStorage.getItem("authToken")) {
+    const authToken = localStorage.getItem("authToken");
+    if (authToken) {
       router.push("/");
     } else {
-      return (
-        <form className="max-w-sm mx-auto m-16" onSubmit={handleSubmit}>
-          <div className="mb-5">
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-            >
-              Tu email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              placeholder="petru@pochoclus.com"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="mb-5">
-            <label
-              htmlFor="name"
-              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-            >
-              Nombre de usuario
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              placeholder="Kingbara"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="mb-5">
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-            >
-              Tu contraseña
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="contraseña"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="mb-5">
-            <label
-              htmlFor="repeatPassword"
-              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-            >
-              Repetí tu contraseña
-            </label>
-            <input
-              type="password"
-              id="repeatPassword"
-              name="repeatPassword"
-              placeholder="contraseña"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              value={formData.repeatPassword}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="flex items-start mb-5"></div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Registrarme
-          </button>
-          <h1 className="text-red-600 text-center m-4">{msg}</h1>
-        </form>
-      );
+      setIsLoading(false); 
+    }
+  }, [router]);
+
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      router.push("/");
     }
   });
 
@@ -146,4 +71,93 @@ export default function Signup() {
       setErrorValue(true);
     }
   };
+
+  if (isLoading) {
+    return <p>Cargando...</p>;
+  }
+
+  return (
+    <form className="max-w-sm mx-auto m-16" onSubmit={handleSubmit}>
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+        >
+          Tu email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder="petru@pochoclus.com"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="name"
+          className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+        >
+          Nombre de usuario
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder="Kingbara"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+        >
+          Tu contraseña
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="contraseña"
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="repeatPassword"
+          className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+        >
+          Repetí tu contraseña
+        </label>
+        <input
+          type="password"
+          id="repeatPassword"
+          name="repeatPassword"
+          placeholder="contraseña"
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          value={formData.repeatPassword}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="flex items-start mb-5"></div>
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Registrarme
+      </button>
+      <h1 className="text-red-600 text-center m-4">{msg}</h1>
+    </form>
+  );
 }
