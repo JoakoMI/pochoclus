137abcaf985c3cea66096c2a36e4907e45fb4a17
JoakoMI <joaquinmarchione@gmail.com>
2024-07-26 20:43:22 -0300

    Configuración de CORS

--- pochoclus-api/app.js
+++ pochoclus-api/app.js
@@ -1,19 +1,25 @@
-import 'dotenv/config';
-import express from 'express';
-import moviesRouter from './routes/movies.js';
-import movieCollectionsRouter from './routes/movieCollections.js';
-import usersRouter from './routes/users.js';
-import cors from 'cors';
+import "dotenv/config";
+import express from "express";
+import moviesRouter from "./routes/movies.js";
+import movieCollectionsRouter from "./routes/movieCollections.js";
+import usersRouter from "./routes/users.js";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:3000", // URL del frontend
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/api/movies', moviesRouter);
-app.use('/api/movieCollections', movieCollectionsRouter);
-app.use('/api/users', usersRouter);
+app.use("/api/movies", moviesRouter);
+app.use("/api/movieCollections", movieCollectionsRouter);
+app.use("/api/users", usersRouter);
 
 app.listen(PORT, () => {
-	console.log('Servidor Web en el puerto:', PORT);
+  console.log("Servidor Web en el puerto:", PORT);
 });
